bb8e4c8982d915e9437bc8812baa73c6740fe7f0
Jordan Feigner <jordan.feigner.r@gmail.com>
2021-08-18 18:08:04 -0700

    Converted Grid to interactive object

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -15,7 +15,10 @@ function main() {
     ctx.fillStyle = "rgb(120, 120, 120)";
     ctx.fillRect(0, 0, width, height);
 
-    grid(canvas, ctx, 14, 20, 4, "black");
+    var game_grid = new CanvasGrid(canvas, 7, 7, 4);
+    game_grid.start();
+
+    var stopper = 0;
 }
 
 function grid(canvas, ctx, rows, columns, line_width, line_color) {
@@ -41,5 +44,110 @@ function grid(canvas, ctx, rows, columns, line_width, line_color) {
         ctx.closePath();
 
         ctx.stroke();
+    }
+}
+
+function CanvasGrid(canvas, rows, columns, line_width) {
+    this.ctx = canvas.getContext("2d");
+
+    this.rect = {
+        x: line_width / 2,
+        y: line_width / 2,
+        width: canvas.width - line_width,
+        height: canvas.height - line_width
+    }
+
+    this.rows = rows;
+    this.columns = columns;
+
+    this.cells = [];
+
+    this.createCells = function() {
+        var row_height = this.rect.height / this.rows;
+        var column_width = this.rect.width / this.columns;
+
+        for(var i = 0; i < rows; i++) {
+            for(var j = 0; j < columns; j++) {
+                var cell_rect = {
+                    x: this.rect.x + (i * row_height),
+                    y: this.rect.y + (j * column_width),
+                    width: column_width,
+                    height: row_height
+                }
+                this.cells.push(new CanvasGridCell(canvas, cell_rect, line_width));
+            }
+        }
+    }
+
+    this.drawCells = function() {
+        this.cells.forEach(cell => cell.draw());
+    }
+
+    this.events = function() {
+        canvas.addEventListener("click", event => {
+            var mouse_x = event.offsetX;
+            var mouse_y = event.offsetY;
+
+            this.cells.forEach(cell => {
+                if(isInBounds(mouse_x, mouse_y, cell.rect)) {
+                    cell.ping();
+                    setTimeout(() => {
+                        cell.draw();
+                    }, 100);
+                }
+            })
+        })
+    }
+
+    this.start = function() {
+        this.createCells();
+        this.drawCells();
+        this.events();
+    }
+}
+
+function CanvasGridCell(canvas, rect, line_width) {
+    this.ctx = canvas.getContext("2d");
+
+    this.rect = {
+        x: rect.x,
+        y: rect.y,
+        width: rect.width,
+        height: rect.height
+    }
+
+    this.line_width = line_width;
+
+    this.draw = function() {
+        this.ctx.lineWidth = this.line_width;
+        this.ctx.strokeStyle = "black";
+
+        this.ctx.beginPath();
+        this.ctx.rect(this.rect.x, this.rect.y, this.rect.width, this.rect.height);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+
+    this.ping = function() {
+        this.ctx.fillStyle = "red";
+
+        this.ctx.beginPath();
+        this.ctx.rect(this.rect.x, this.rect.y, this.rect.width, this.rect.height);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+}
+
+// Checks if x,y pair falls within bounds of rect object
+// Expects rect = {x: , y: , width: , height: };
+function isInBounds(x, y, rect) {
+    var x_lower = rect.x
+    var x_upper = rect.x + rect.width;
+    var y_lower = rect.y;
+    var y_upper = rect.y + rect.height;
+    if(x > x_lower && x < x_upper && y > y_lower && y < y_upper) {
+        return true;
+    } else {
+        return false;
     }
 }
